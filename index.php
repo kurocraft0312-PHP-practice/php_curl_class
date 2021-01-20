9e4b5af14803b831bfc45cff64a1be44b5c4b804
kurocraft0312 <kurocraft0312@gmail.com>
2021-01-20 09:05:44 +0900

    add:単一のcURL処理をテスト

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$sURL = "https://news.yahoo.co.jp/rss/topics/it.xml";
+
+$conn = curl_init();
+curl_setopt($conn,CURLOPT_URL,$sURL);
+curl_setopt($conn,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($conn,CURLOPT_TIMEOUT,5);
+curl_setopt($conn,CURLOPT_HEADER,false);
+$ret = curl_exec($conn);
+curl_close($conn);
+
+// simplexml_load_string: XML文字列をオブジェクトに代入する
+$xml = simplexml_load_string($ret);
+// MEMO:API内にある<channel>要素の中にある<item>要素から<title>要素を取得している
+foreach($xml->channel->item as $item) {
+    echo (string)$item->title."\n";
+}
+?>
